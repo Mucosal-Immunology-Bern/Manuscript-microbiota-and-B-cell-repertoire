8a3a0420aaaa38ae96bdfa222cd7e89b9cf1a5e5
JLimen <jlimenitakis@gmail.com>
2020-07-21 16:04:10 +0200

    Update example_code_preprocessing_UMI_with_presto.sh

--- Scripts/example_code_preprocessing_UMI_with_presto.sh
+++ Scripts/example_code_preprocessing_UMI_with_presto.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#Example proccedure to pre-process repseq amplicon sequencing tagged with UMIs with pRESTO (documentation: https://presto.readthedocs.io/en/stable/)
+#Example bash script to pre-process repseq amplicon sequencing tagged with UMIs with pRESTO (documentation: https://presto.readthedocs.io/en/stable/)
 
 gunzip -c UMIsample_R1.fastq.gz > UMIsample_R1.fastq
 gunzip -c UMIsample_R2.fastq.gz > UMIsample_R2.fastq
@@ -31,3 +31,8 @@ CollapseSeq.py -s UMIsample*reheader.fastq -n 20 --inner --uf PRCONS \
     --cf CONSCOUNT --act sum --outname UMIsample
 SplitSeq.py group -s UMIsample_collapse-unique.fastq -f CONSCOUNT --num 2 --outname UMIsample
 ParseHeaders.py table -s UMIsample_atleast-2.fastq -f ID PRCONS CONSCOUNT DUPCOUNT
+
+#Example bash commands to pass pRESTO's output to MIXCR 
+mixcr align -c IGH -s mmu -r alignmentReport_UMIsample.txt UMIsample_collapse-unique.fastq alignments_UMIsample.vdjca
+mixcr assemble -r assembleReport_UMIsample.txt -OcloneClusteringParameters=null alignments_UMIsample.vdjca clones_UMIsample.clns
+mixcr exportClones clones_UMIsample.clns clones_UMIsample.txt
